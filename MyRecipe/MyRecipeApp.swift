4f2df8d6b0fb0f4dbcb0a6f42e032bde3ae50aa6
Atakan Başaran <basaranatakan10@gmail.com>
2023-11-29 01:41:32 +0300

    Initial Commit

---
+++ MyRecipe/MyRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeApp.swift
+//  MyRecipe
+//
+//  Created by Atakan Başaran on 29.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
